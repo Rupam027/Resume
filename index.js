0eee4ab83f7699e63e09a8e435770ea6492f05b1
Rupam027 <www.rupamdasgupta@gmail.com>
2022-02-10 16:31:27 +0530

    resume created

---
+++ index.js
@@ -0,0 +1,19 @@
+function go_to_s1(){
+        var pos = document.getElementById("section-1").offsetTop ; 
+        window.scrollTo(0 , pos) ; 
+}
+
+function go_to_s2(){
+        var pos = document.getElementById("section-2").offsetTop ; 
+        window.scrollTo(0 , pos) ; 
+}
+
+function go_to_s3(){
+        var pos = document.getElementById("section-3").offsetTop ; 
+        window.scrollTo(0 , pos) ; 
+}
+
+function go_to_s4(){
+        var pos = document.getElementById("section-4").offsetTop ; 
+        window.scrollTo(0 , pos) ; 
+}
